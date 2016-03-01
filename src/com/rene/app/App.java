4846b1868b2c2f72f5e0ee11bc062e4d37fd0b17
renekeijzer <rene.keijzer@casema.nl>
2016-03-01 10:38:49 +0100

    impimproved mapping 15 min > 5sec

--- src/com/rene/app/App.java
+++ src/com/rene/app/App.java
@@ -54,15 +54,19 @@ class FriendlyNumberMapper extends Mapper<LongWritable, Text, Text, IntWritable>
 	
   private static int sumFactors(int n)
   {
-      int sum = 0;
-      for (int div=1; div<=n/2; div++)
-      {
-          if (n%div == 0)
-          {
-              sum += div;
-          }
-      }
-      return sum;
+	  int maxD = (int)Math.sqrt(n);
+	  int sum=1;
+	  for(int i = 2; i <= maxD; i++)
+	  {
+		  if(n % i == 0)
+		  {
+			  sum += i;
+			  int d = n/i;
+			  if(d!=i)
+				  sum+=d;
+		  }
+	  }
+	  return sum;
   }
 
 
